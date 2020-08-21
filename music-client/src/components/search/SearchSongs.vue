12667e8ae8e238e2833f0212c6b49733f6ef3579
290768967@qq.com <dingkai@lajsf.com>
2020-08-21 11:46:36 +0800

    提交

---
+++ music-client/src/components/search/SearchSongs.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search-songs">
+    <album-content :songList="listOfSongs"></album-content>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import AlbumContent from '../AlbumContent'
+
+export default {
+  name: 'search-songs',
+  components: {
+    AlbumContent
+  },
+  computed: {
+    ...mapGetters([
+      'listOfSongs' // 存放的音乐
+    ])
+  },
+  mounted () {
+    this.getSong()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/css/search-songs.scss';
+</style>
